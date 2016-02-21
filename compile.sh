9b69b7991112ab0986db79d3151159f410332eae
dankernel <google@dankook.ac.kr>
2016-02-21 23:57:08 +0900

    first commit

---
+++ compile.sh
@@ -0,0 +1,3 @@
+#gcc -finput-charset=UTF-8 -o main main.c
+gcc -finput-charset=UTF-8 -g -lm -O4 -o main main.c
+./main
---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2016년 02월 21일 23시 53분 50초
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+int main(int argc, const char *argv[])
+{
+  printf("Test\n");
+  return 0;
+}
